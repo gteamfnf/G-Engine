c19d17c7a6481ac84b131f1de77d9f121f38f7ab
Brainy7980 <bpurdy12@icloud.com>
2026-02-02 09:37:23 -0800

    Implement IConductor in BaseStage class

--- source/game/play/stages/BaseStage.hx
+++ source/game/play/stages/BaseStage.hx
@@ -1,3 +1,30 @@
 package game.play.stages;
 
-class BaseStage extends FlxBasic {}
+import backend.conduct.*;
+
+class BaseStage extends FlxBasic implements IConductor
+{
+    public var stage:String = 'default';
+
+    override public function new(stage:String)
+    {
+        this.stage = stage;
+
+        super();
+        conductor = new Conductor(this);
+        controls = new Controls();
+    }
+
+    override public function update(elapsed:Float) 
+    {
+        super.update(elapsed);
+
+        conductor.update(elapsed);
+    }
+
+    public function stepHit(curStep:Int) {}
+
+    public function beatHit(curBeat:Int) {}
+
+    public function measureHit(curMeasure:Int) {}
+}
