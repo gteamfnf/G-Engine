932d4be615ee3dcc6015f7d8122f4261381dc7a2
Brainy7980 <bpurdy12@icloud.com>
2026-02-02 14:34:02 -0800

    Add save binding :)

--- source/game/menus/states/InitState.hx
+++ source/game/menus/states/InitState.hx
@@ -4,6 +4,7 @@ class InitState extends FlxState
 {
     override public function create()
     {
+				FlxG.save.bind("GEngineTeam", "funkin");
         FlxG.switchState(new game.menus.states.TitleState());
     }
 }
