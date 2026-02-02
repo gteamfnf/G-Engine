566a17bc6b89a61ad0f9c8958fd226161e3064af
Brainy7980 <bpurdy12@icloud.com>
2026-02-02 09:05:46 -0800

    Add logo print using SimpleMacros in Main class

--- source/Main.hx
+++ source/Main.hx
@@ -2,12 +2,26 @@ package;
 
 import flixel.FlxGame;
 import openfl.display.Sprite;
-
 import game.menus.states.InitState;
+import backend.macros.SimpleMacros;
+
 class Main extends Sprite
 {
 	public function new()
 	{
+		SimpleMacros.macroPrint("
+ $$$$$$\        $$$$$$$$\                     $$\                     
+$$  __$$\       $$  _____|                    \__|                    
+$$ /  \__|      $$ |      $$$$$$$\   $$$$$$\  $$\ $$$$$$$\   $$$$$$\  
+$$ |$$$$\       $$$$$\    $$  __$$\ $$  __$$\ $$ |$$  __$$\ $$  __$$\ 
+$$ |\_$$ |      $$  __|   $$ |  $$ |$$ /  $$ |$$ |$$ |  $$ |$$$$$$$$ |
+$$ |  $$ |      $$ |      $$ |  $$ |$$ |  $$ |$$ |$$ |  $$ |$$   ____|
+\$$$$$$  |      $$$$$$$$\ $$ |  $$ |\$$$$$$$ |$$ |$$ |  $$ |\$$$$$$$\ 
+ \______/       \________|\__|  \__| \____$$ |\__|\__|  \__| \_______|
+                                    $$\   $$ |                        
+                                    \$$$$$$  |                        
+                                     \______/       
+");
 		super();
 		addChild(new FlxGame(0, 0, InitState, 60, 60, true));
 	}
