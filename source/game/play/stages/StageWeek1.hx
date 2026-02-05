10dba3072dac276abbff200784a6a00da06f7396
Brainy0789 <bpurdy12@icloud.com>
2026-02-05 14:21:24 -0800

    Change to funkin sprite :3

--- source/game/play/stages/StageWeek1.hx
+++ source/game/play/stages/StageWeek1.hx
@@ -10,22 +10,22 @@ class StageWeek1 extends BaseStage
 
 	override function create()
 	{
-		var bg:FlxSprite = new FlxSprite(-600, -200, Paths.getImage('week1/stageback'));
+		var bg:FunkinSprite = new FunkinSprite(-600, -200, Paths.getImage('week1/stageback'));
 		bg.scrollFactor.set(0.9, 0.9);
 		add(bg);
 
-		var stageFront:FlxSprite = new FlxSprite(-650, 600, Paths.getImage('week1/stagefront'));
+		var stageFront:FunkinSprite = new FunkinSprite(-650, 600, Paths.getImage('week1/stagefront'));
 		stageFront.setGraphicSize(Std.int(stageFront.width * 1.1));
 		stageFront.updateHitbox();
 		add(stageFront);
 
-		var stageLight:FlxSprite = new FlxSprite(-125, -100, Paths.getImage('week1/stage_light'));
+		var stageLight:FunkinSprite = new FunkinSprite(-125, -100, Paths.getImage('week1/stage_light'));
 		stageLight.setGraphicSize(Std.int(stageLight.width * 1.1));
 		stageLight.updateHitbox();
 		stageLight.scrollFactor.set(0.9, 0.9);
 		add(stageLight);
 
-		var stageLight:FlxSprite = new FlxSprite(1225, -100, Paths.getImage('week1/stage_light'));
+		var stageLight:FunkinSprite = new FunkinSprite(1225, -100, Paths.getImage('week1/stage_light'));
 		stageLight.setGraphicSize(Std.int(stageLight.width * 1.1));
 		stageLight.updateHitbox();
 		stageLight.flipX = true;
@@ -39,7 +39,7 @@ class StageWeek1 extends BaseStage
 
 	override function createPost()
 	{
-		var stageCurtains:FlxSprite = new FlxSprite(-500, -300, Paths.getImage('week1/stagecurtains'));
+		var stageCurtains:FunkinSprite = new FunkinSprite(-500, -300, Paths.getImage('week1/stagecurtains'));
 		stageCurtains.setGraphicSize(Std.int(stageCurtains.width * 1.1));
 		stageCurtains.updateHitbox();
 		stageCurtains.scrollFactor.set(1.3, 1.3);
